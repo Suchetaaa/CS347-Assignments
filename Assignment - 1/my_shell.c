52b2dcf5d73e558a211efd50ab7ff587bdcfb981
Suchetaaa <sucheta317@gmail.com>
2019-02-15 18:58:53 +0530

    part b done

--- Assignment - 1/my_shell.c
+++ Assignment - 1/my_shell.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
@@ -11,6 +13,11 @@
 int my_shell_cd(char **Tokens, int background);
 int my_shell_ls(char **Tokens, int background);
 int my_shell_others(char **Tokens, int background);
+
+pid_t child_pid;
+pid_t fg_group;
+bool fg_group_set = 0;
+pid_t bg_group; 
 
 /* Splits the string by space and returns the array of tokens
 *
@@ -86,9 +93,10 @@ int my_shell_ls(char **Tokens, int background)
 		if (check == 0) 
 		{
 			int check_exec;
+			if (!fg_group_set) fg_group = setsid();
+			if (fg_group_set) setpgid(getpid(), fg_group);
 			check_exec = execvp(Tokens[0], Tokens);
 			if (check_exec == -1) perror("Exec failed");
-			else if (!background) printf("%s\n","Shell: Background process finished" );
 			exit(0);
 		}
 
@@ -100,7 +108,7 @@ int my_shell_ls(char **Tokens, int background)
 
 		else
 		{
-			if (!background) wait(NULL);
+			wait(NULL);
 		}
 	}
 	
@@ -126,11 +134,12 @@ int my_shell_others(char **Tokens, int background)
 		if (check == 0) 
 		{
 			int check_exec;
+			if (!fg_group_set) fg_group = setsid();
+			if (fg_group_set) setpgid(getpid(), fg_group);
 			//printf("%s\n","HI3" );
 			check_exec = execvp(Tokens[0], (Tokens));
 			//printf("%d\n", check_exec );
 			if (check_exec == -1) perror("Exec failed");
-			else if (!background) printf("%s\n","Shell: Background process finished" );
 			exit(0);
 		}
 
@@ -142,12 +151,19 @@ int my_shell_others(char **Tokens, int background)
 
 		else
 		{
-			if (!background) wait(NULL);
+			wait(NULL);
 			return 0;
 		}
 	}
 	
 	return 0;
+}
+
+void wait_bg(int signal)
+{
+	int status;
+	waitpid(child_pid, &status, 0);
+	printf("%s\n", "Shell: Background process has terminated" );
 }
 
 int commands_to_int(char *command)
@@ -173,8 +189,14 @@ int main(int argc, char* argv[]) {
 			return -1;
 		}
 	}
+	int background = 0;
 
-	while(1) {			
+	
+
+
+	while(1) {	
+		int status;
+		
 		/* BEGIN: TAKING INPUT */
 		bzero(line, sizeof(line));
 		if(argc == 2) { // batch mode
@@ -188,13 +210,15 @@ int main(int argc, char* argv[]) {
 			getchar();
 		}
 
-		int background = 0;
+		
 		//printf("Command entered: %s\n", line);
 
 		/* END: TAKING INPUT */
 
 		line[strlen(line)] = '\n'; //terminate with new line
 		tokens = tokenize(line);
+
+		// printf("%d\n", getpid() );
    
        //do whatever you want with the commands, here we just print them
 		int j = 0, k = 0;
@@ -205,12 +229,16 @@ int main(int argc, char* argv[]) {
 			( (strcmp(tokens[0], "ls") == 0) && (strcmp(tokens[1], "&&&") == 0) ) 
 			)
 		{
+			// printf("%d\n", getpid() );
+
 			while (tokens[0] != NULL)
 			{
 				// printf("%s\n", "Loop" );
 				int identifier = commands_to_int(tokens[0]);
 				tokens[0 + 1 + identifier%2] = NULL;
 				// funcs[identifier](tokens, background);
+
+
 
 				if (strcmp(tokens[0], "cd") == 0) 
 				{
@@ -224,6 +252,8 @@ int main(int argc, char* argv[]) {
 
 					if (fork() == 0)
 					{
+						if (!fg_group_set) fg_group = setsid();
+						if (fg_group_set) setpgid(getpid(), fg_group);
 						if (execvp(tokens[0], tokens) != 0) perror("Failure");
 					}
 				}
@@ -281,31 +311,79 @@ int main(int argc, char* argv[]) {
 			}	
 		}
 
-		// if (
-		// 	( ( (strcmp(tokens[0], "cd") == 0) || (strcmp(tokens[0], "echo") == 0) || (strcmp(tokens[0], "cat") == 0) || (strcmp(tokens[0], "sleep") == 0) ) && 
-		// 		(strcmp(tokens[2], "&") == 0) ) 
-		// 	|| 
-		// 	( (strcmp(tokens[0], "ls") == 0) && (strcmp(tokens[1], "&") == 0) ) 
-		// 	)
-		// {
-		// 	background = 1;
-		// 	int identifier = commands_to_int(tokens[0]);
-		// 	printf("%s\n", "Background mode");
-		// 	funcs[identifier](tokens, background);
-
-		// // 	int i = 0;
+		else if (
+			( ( (strcmp(tokens[0], "cd") == 0) || (strcmp(tokens[0], "echo") == 0) || (strcmp(tokens[0], "cat") == 0) || (strcmp(tokens[0], "sleep") == 0) ) && 
+				(strcmp(tokens[2], "&") == 0) ) 
+			|| 
+			( (strcmp(tokens[0], "ls") == 0) && (strcmp(tokens[1], "&") == 0) ) 
+			)
+		{
 			
+			background = 1;
+			
+			int identifier = commands_to_int(tokens[0]);
+			printf("%s\n", "Background mode");
+			
+			if (strcmp(tokens[0], "cd") == 0) 
+				{
+					if (chdir(tokens[1]) != 0) perror("Error");
+					else printf("%s\n", "&&& cd success" );
+				}
 
-		// }
-		// else if (strcmp(tokens[0], "cd") == 0) my_shell_cd(tokens);
-		// else if (strcmp(tokens[0], "ls") == 0) my_shell_ls(tokens);
-		// else if (strcmp(tokens[0], "cat") == 0) my_shell_others(tokens);
-		// else if (strcmp(tokens[0], "sleep") == 0) my_shell_others(tokens);
-		// else if (strcmp(tokens[0], "echo") == 0) my_shell_others(tokens);
-		// else 
+			else if (strcmp(tokens[0], "ls") == 0 || strcmp(tokens[0], "echo") == 0 || strcmp(tokens[0], "sleep") == 0 || strcmp(tokens[0], "cat") == 0)
+			{
+				printf("%s\n", "Reached here!" );
+
+				int identifier = commands_to_int(tokens[0]);
+				tokens[0 + 1 + identifier%2] = NULL;
+
+				child_pid = fork();
+				if (child_pid == 0)
+				{
+					// printf("%d\n", child_pid );
+					// if (!fg_group_set) fg_group = setsid();
+					// if (fg_group_set) setpgid(getpid(), fg_group);
+					int check = execvp(tokens[0], tokens);
+					if (check != 0) perror("Failure");
+					//exit(0);
+				}
+
+				if (child_pid == -1) perror("Fork failed");
+
+				else printf("%s\n", "Next" );;
+
+
+			}
+
+		}
+
+
+		// if (background == 1)
 		// {
-		// 	printf("%s\n", "Shell: Incorrect command" );
+		// 	printf("%s\n", "Terminate" );
+		// 	printf("%d\n", child_pid );
+		// 	waitpid(child_pid, &status, WNOHANG);
+		// 	if (status) printf("%s\n","Shell: Background process has been terminated" );
 		// }
+		// printf("%s\n",tokens[0] );
+		// else if (strcmp(tokens[0], "cd") == 0) my_shell_cd(tokens, background);
+		// else if (strcmp(tokens[0], "ls") == 0) my_shell_ls(tokens, background);
+		// else if (strcmp(tokens[0], "cat") == 0) my_shell_others(tokens, background);
+		// else if (strcmp(tokens[0], "sleep") == 0) my_shell_others(tokens, background);
+		// else if (strcmp(tokens[0], "echo") == 0) my_shell_others(tokens, background);
+		else 
+		{
+			printf("%s\n", "Shell: Incorrect command" );
+		}
+
+		signal(SIGCHLD, wait_bg);
+
+
+		// printf("%d\n", fg_group );
+		// killpg(fg_group, SIGINT);
+
+		
+
 
 
 
@@ -315,11 +393,13 @@ int main(int argc, char* argv[]) {
        
 		// Freeing the allocated memory	
 		// printf("%s\n", "Out" );
-		// for(i=0;tokens[i]!=NULL;i++){
-		// 	free(tokens[i]);
-		// }
+		printf("%d\n", status );
+		for(i=0;tokens[i]!=NULL;i++){
+			free(tokens[i]);
+		}
 		// free(tokens);
 
 	}
+
 	return 0;
 }
